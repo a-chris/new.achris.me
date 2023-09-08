72147f7f5ad61c01b1c5c65e410cc2d6def20629
christian-toscano <toscano@extendi.it>
2023-09-08 20:41:12 +0200

    update bridgetown configs for > 1.3

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,5 +1,5 @@
-import "index.scss"
-import "syntax-highlighting.css"
+import "$styles/index.scss"
+import "$styles/syntax-highlighting.css"
 
 // Import all JavaScript & CSS files from src/_components
 import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
